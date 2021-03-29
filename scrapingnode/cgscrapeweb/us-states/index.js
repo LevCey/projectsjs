04813b5f277c5a1525190db9a0f0aad613dc5a54
Your Name <vrkafa@gmail.com>
2021-03-29 14:18:58 +0300

    parse table elements

--- scrapingnode/cgscrapeweb/us-states/index.js
+++ scrapingnode/cgscrapeweb/us-states/index.js
@@ -1,12 +1,21 @@
 // Coding Garden https://youtu.be/76d2yOMXmag
 
-const axios = require("axios");
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 const page_url = "https://en.wikipedia.org/wiki/List_of_states_and_territories_of_the_United_States";
 
 async function getUSStates() {
     const { data } = await axios.get(page_url);
-    console.log(data);
+    const $ = cheerio.load(data);
+    const table = $('caption:contains("States of the United States of America")').parent();
+    const body = table.find('tbody tr').each((i, element) => {
+        const $element = $(element);
+        const state = {};
+        state.name = $element.find('th').text();
+        console.log(state);
+
+    });
 
 }
 
