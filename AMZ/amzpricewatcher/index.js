318245ec5eeb94a43074efe0541660f0c0edaa4d
Your Name <vrkafa@gmail.com>
2021-04-04 12:53:19 +0300

    cron job

--- AMZ/amzpricewatcher/index.js
+++ AMZ/amzpricewatcher/index.js
@@ -13,13 +13,38 @@ const fetchPrice = async (url, targetPrice) => {
     console.log(price);
 
     if (targetPrice >= price) {
-        console.log('cheap enough');
+        sendEmail(url, price);
     } else {
         console.log('too expensive');
     }
 }
 
-fetchPrice(
+const sendEmail = async (url, price) => {
+    // const testAccount = await nodemailer.createTestAccount();
+    const transport = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        //secure: false,
+        auth: {
+            user: 'nannie.yundt@ethereal.email',
+            pass: 'nvK4GZRR5CW2xgMuNF'
+        }
+    });
+    const info = await transport.sendMail({
+        from: 'nannie.yundt@ethereal.email',
+        to: 'nannie.yundt@ethereal.email',
+        subject: 'amazon watcher',
+        text: `${price} - ${url}`,
+        html: `<p>${price}</p> <p>${url}</p>`,
+    });
+    console.log(nodemailer.getTestMessageUrl());
+}
+
+const watchPrice = (priceTarget, url, schedule = '*/5 * * * * *') => {
+    cron.schedule(schedule, () => fetchPrice(url, priceTarget));
+};
+
+watchPrice(
     'https://www.amazon.co.uk/Echo-Dot-3rd-Gen-Charcoal/dp/B07PJV3JPR',
     49.99,
 );
