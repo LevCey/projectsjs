68197759fd6eb2667c4a3195106d2f91742b6848
Your Name <vrkafa@gmail.com>
2021-04-06 17:52:25 +0300

    bootsrap added

--- nodeexpress/randomnamegenerator/public/app.js
+++ nodeexpress/randomnamegenerator/public/app.js
@@ -3,6 +3,7 @@ const loadRandomName = (resultDiv) => {
     fetch('http://localhost:3001/random-name')
         .then(response => response.json())
         .then(result => {
+            resultDiv.classList.add('alert', 'alert-success');
             resultDiv.innerHTML = `<h2>${result.first_name} ${result.last_name}</h2>`;
         });
 };
