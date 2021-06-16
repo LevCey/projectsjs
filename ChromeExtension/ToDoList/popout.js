985109461924bebab806b33cdad112b9fce32282
Your Name <vrkafa@gmail.com>
2021-06-16 12:50:08 +0300

    localstorage setitems and fetch itms

--- ChromeExtension/ToDoList/popout.js
+++ ChromeExtension/ToDoList/popout.js
@@ -1,3 +1,9 @@
+/**
+ *  Consolda bu çalıştırıldı ve local storage kaydedildi => 
+ *  localStorage.setItem('todo-items', '[{"item":"Record next video","status":0},{"item":"Record next video","status":1}]')
+ */
+
+
 
 const items = [{ "item": "Record next video", "status": 0 },
 { "item": "Record next video", "status": 1 }];
@@ -9,18 +15,25 @@ console.log(itemsStr);
 
 function fetchItems() {
     const itemsList = document.querySelector('ul.todo-items');
-    itemsList.innerHTML = '';
+    itemsList.innerHTML = ''; // liste temizlendi.
     var newItemHTML = '';
     try {
         var items = localStorage.getItem('todo-items');
         var itemsArr = JSON.parse(items);
 
         for (let i = 0; i < itemsArr.length; i++) {
-            newItemHTML += `<li data-itemindex="${i}">
+            console.log('local storage items length= ' + itemsArr.length);
+            var status = '';
+            if (itemsArr[i].status == 1) {
+                status = 'class="done"';
+            }
+            newItemHTML += `<li data-itemindex="${i}" ${status}>
             <span class="item">${itemsArr[i].item}</span> 
             <div><span>✅</span><span>🗑</span></div>
             </li>`;
         }
+
+        itemsList.innerHTML = newItemHTML;
 
     } catch (error) {
         console.log(error);
@@ -31,3 +44,5 @@ function saveItems(obj) {
     var string = JSON.stringify(obj);
     localStorage.setItem('todo-items', string);
 }
+
+fetchItems();
