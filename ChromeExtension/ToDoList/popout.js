a24c13aa6bae5c1741e3188a7ceb3b7782ae8b1c
Your Name <vrkafa@gmail.com>
2021-06-16 14:01:12 +0300

    save

--- ChromeExtension/ToDoList/popout.js
+++ ChromeExtension/ToDoList/popout.js
@@ -18,26 +18,64 @@ function fetchItems() {
     itemsList.innerHTML = ''; // liste temizlendi.
     var newItemHTML = '';
     try {
-        var items = localStorage.getItem('todo-items');
-        var itemsArr = JSON.parse(items);
+        var itemsStorage = localStorage.getItem('todo-items');
+        var itemsArr = JSON.parse(itemsStorage);
 
         for (let i = 0; i < itemsArr.length; i++) {
             console.log('local storage items length= ' + itemsArr.length);
             var status = '';
+
             if (itemsArr[i].status == 1) {
                 status = 'class="done"';
             }
+
             newItemHTML += `<li data-itemindex="${i}" ${status}>
             <span class="item">${itemsArr[i].item}</span> 
-            <div><span>✅</span><span>🗑</span></div>
+            <div><span class="itemComplete">✅</span><span class="itemDelete">🗑</span></div>
             </li>`;
         }
 
         itemsList.innerHTML = newItemHTML;
 
+        var itemsListUL = document.querySelectorAll('ul li');
+        for (let i = 0; index < itemsListUL.length; i++) {
+
+            itemsListUL[i].querySelector('.itemComplete').addEventListener('click', function () {
+                //
+                var index = this.parentNode.parentNode.dataset.itemindex;
+                itemComplete(index);
+
+            });
+            itemsListUL[i].querySelector('.itemDelete').addEventListener('click', function () {
+                //
+
+                var index = this.parentNode.parentNode.dataset.itemindex;
+                itemDelete(index);
+
+            });
+        }
+
     } catch (error) {
         console.log(error);
     }
+}
+
+function itemComplete(index) {
+    var itemsStorage = localStorage.getItem('todo-items');
+    var itemsArr = JSON.parse(itemsStorage);
+    itemsArr[index].status = 1;
+    saveItems(itemsArr);
+
+    document.querySelector('ul.todo-items li[data-itemindex="' + index + '"]').className = 'done';
+}
+
+function itemDelete(index) {
+    var itemsStorage = localStorage.getItem('todo-items');
+    var itemsArr = JSON.parse(itemsStorage);
+
+    itemsArr.splice(index, 1);
+    saveItems(itemsArr);
+    document.querySelector('ul.todo-items li[data-itemindex="' + index + '"]').remove;
 }
 
 function saveItems(obj) {
