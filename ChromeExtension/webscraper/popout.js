f427f959299714f36b9eaa1e87ef5f6c8d901b85
Your Name <vrkafa@gmail.com>
2021-06-20 08:26:25 +0300

    popout ui design

--- ChromeExtension/webscraper/popout.js
+++ ChromeExtension/webscraper/popout.js
@@ -1,86 +1,43 @@
-/**
- *  Consolda bu çalıştırıldı ve local storage kaydedildi => 
- *  localStorage.setItem('todo-items', '[{"item":"Record next video","status":0},{"item":"Record next video","status":1}]')
- */
 
 
-
-const items = [{ "item": "Record next video", "status": 0 },
-{ "item": "Record next video", "status": 1 }];
-
-const itemsStr = JSON.stringify(items);
-
-console.log(items);
-console.log(itemsStr);
-
-function fetchItems() {
-    const itemsList = document.querySelector('ul.todo-items');
-    itemsList.innerHTML = ''; // liste temizlendi.
-    var newItemHTML = '';
-    try {
-        var itemsStorage = localStorage.getItem('todo-items');
-        var itemsArr = JSON.parse(itemsStorage);
-
-        for (let i = 0; i < itemsArr.length; i++) {
-            console.log('local storage items length= ' + itemsArr.length);
-            var status = '';
-
-            if (itemsArr[i].status == 1) {
-                status = 'class="done"';
-            }
-
-            newItemHTML += `<li data-itemindex="${i}" ${status}>
-            <span class="item">${itemsArr[i].item}</span> 
-            <div><span class="itemComplete">✅</span><span class="itemDelete">🗑</span></div>
-            </li>`;
-        }
-
-        itemsList.innerHTML = newItemHTML;
-
-        var itemsListUL = document.querySelectorAll('ul li');
-        for (let i = 0; index < itemsListUL.length; i++) {
-
-            itemsListUL[i].querySelector('.itemComplete').addEventListener('click', function () {
-                //
-                var index = this.parentNode.parentNode.dataset.itemindex;
-                itemComplete(index);
-
-            });
-            itemsListUL[i].querySelector('.itemDelete').addEventListener('click', function () {
-                //
-
-                var index = this.parentNode.parentNode.dataset.itemindex;
-                itemDelete(index);
-
-            });
-        }
-
-    } catch (error) {
-        console.log(error);
+function createCommandObject() {
+    var commandsArr = [];
+    var commands = document.querySelectorAll('.commands-list .command-item');
+    for (let i = 0; i < commands.length; i++) {
+        var itemObj = {};
+        itemObj.type = commands[i].querySelector('select').value;
+        itemObj.one = commands[i].querySelector('.value-1').value;
+        itemObj.two = commands[i].querySelector('.value-2').value;
+        commandsArr.push(itemObj);
     }
+
+    console.log(commandsArr);
 }
 
-function itemComplete(index) {
-    var itemsStorage = localStorage.getItem('todo-items');
-    var itemsArr = JSON.parse(itemsStorage);
-    itemsArr[index].status = 1;
-    saveItems(itemsArr);
 
-    document.querySelector('ul.todo-items li[data-itemindex="' + index + '"]').className = 'done';
-}
+document.querySelector('.run-command').addEventListener('click', function () {
+    createCommandObject();
 
-function itemDelete(index) {
-    var itemsStorage = localStorage.getItem('todo-items');
-    var itemsArr = JSON.parse(itemsStorage);
+});
 
-    itemsArr.splice(index, 1);
-    saveItems(itemsArr);
-    document.querySelector('ul.todo-items li[data-itemindex="' + index + '"]').remove;
-}
+document.querySelector('.new-command').addEventListener('click', function () {
+    var newItem = `<div class="command-item">
+    <select>
+      <option value="wait">Wait</option>
+      <option value="click">Click</option>
+      <option value="enter">Enter</option>
+      <option value="save">Save</option>
+    </select>
+    <input class="value-1" placeholder="200ms" />
+    <input class="value-2" placeholder="Optional" />
+  </div>`;
+    document.querySelector('.commands-list').innerHTML += newItem;
+});
 
-function saveItems(obj) {
-    var string = JSON.stringify(obj);
-    localStorage.setItem('todo-items', string);
-}
 
-fetchItems();
+chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+
+    var activeTab = tabs[0];
+    var obj = {};
+
+});
