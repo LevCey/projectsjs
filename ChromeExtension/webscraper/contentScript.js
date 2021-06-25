9075de195d41b62b068acd2b9a3f8cec553a3e97
Your Name <vrkafa@gmail.com>
2021-06-25 20:22:03 +0300

    compleated

---
+++ ChromeExtension/webscraper/contentScript.js
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+
+chrome.runtime.onMessage.addListener((msg, sender, response) => {
+
+    if (msg.command == "runCommands") {
+        var scrapeObj = msg.data;
+        getNextItem(scrapeObj, 0);
+
+        window.ScraperExt = [];
+    }
+
+});
+
+function getNextItem(obj, index) {
+
+    if (typeof obj[index] !== 'undefined') {
+
+        if (obj[index].type == 'click') {
+            clickEvent(obj, index);
+        }
+        if (obj[index].type == 'wait') {
+            waitEvent(obj, index);
+        }
+        if (obj[index].type == 'save') {
+            saveEvent(obj, index);
+        }
+        if (obj[index].type == 'enter') {
+            enterEvent(obj, index);
+        }
+
+    } else {
+        // send a return
+        alert('commands have been run');
+        chrome.runtime.sendMessage({ command: "run-complete", data: window.ScraperExt });
+
+    }
+
+}
+
+function waitEvent(obj, index) {
+    var item = obj[index];
+    var waitTime = parseInt(item.one);
+    setTimeout(function () {
+        getNextItem(obj, (index + 1));
+    }, item.one);
+
+}
+
+function clickEvent(obj, index) {
+    var item = obj[index];
+    document.querySelector(item.one).click();
+    getNextItem(obj, (index + 1));
+
+}
+
+function saveEvent(obj, index) {
+    var item = obj[index];
+    var value = document.querySelector(item.one).innerText;
+    window.ScraperExt.push(value);
+    getNextItem(obj, (index + 1));
+}
+
+function enterEvent(obj, index) {
+    var item = obj[index];
+    var value = document.querySelector(item.one).value = item.two;
+    window.ScraperExt.push(value);
+    getNextItem(obj, (index + 1));
+}
--- ChromeExtension/webscraper/popout.js
+++ ChromeExtension/webscraper/popout.js
@@ -1,7 +1,16 @@
 
+chrome.runtime.onMessage.addListener((msg, sender, response) => {
+    if (msg.command == 'run-complete') {
+        document.querySelector('textarea').value = JSON.stringify(msg.data);
+        document.querySelector('textarea').style.display = 'block';
+        alert('commands have been run');
+    }
+});
+
 
 function createCommandObject() {
     var commandsArr = [];
+
     var commands = document.querySelectorAll('.commands-list .command-item');
     for (let i = 0; i < commands.length; i++) {
         var itemObj = {};
@@ -11,7 +20,12 @@ function createCommandObject() {
         commandsArr.push(itemObj);
     }
 
-    console.log(commandsArr);
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        var activeTabs = tabs[0];
+        var obj = commandsArr;
+        console.log(activeTabs, obj);
+        chrome.tabs.sendMessage(activeTabs.id, { command: "runCommands", data: obj });
+    });
 }
 
 
