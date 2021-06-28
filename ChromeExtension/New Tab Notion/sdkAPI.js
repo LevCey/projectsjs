3387d7f69d64a600d6f6f22b64a9e35077904735
Your Name <vrkafa@gmail.com>
2021-06-28 22:46:48 +0300

    firebase sdk

---
+++ ChromeExtension/New Tab Notion/sdkAPI.js
@@ -0,0 +1,19 @@
+< !--The core Firebase JS SDK is always required and must be listed first-- >
+
+
+< !--TODO: Add SDKs for Firebase products that you want to use
+https://firebase.google.com/docs/web/setup#available-libraries -->
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyC7K30FUJm6VxBpCFieJRnXJibHNcrjYzU",
+  authDomain: "new-tab-notes-20b54.firebaseapp.com",
+  projectId: "new-tab-notes-20b54",
+  storageBucket: "new-tab-notes-20b54.appspot.com",
+  messagingSenderId: "518935409152",
+  appId: "1:518935409152:web:7185558637ef8d6cf1dee5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
